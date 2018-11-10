7f05d78936807e0a74660f24b5f86d1c4059b94e
Nicholas Biddell <nicbiddell@gmail.com>
2018-11-10 17:32:14 -0500

    Make program a little more interactive based on http route

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  string
+}
 
 func main() {
-	fmt.Println("vim-go")
+	http.HandleFunc("/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	body := "Hello World"
+
+	page := &Page{Title:title, Body:body}
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
 }
